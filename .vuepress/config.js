cc76d2db95141eeb8481127b96ec73b03c8cb8eb
Enrico Deleo <hello@enricodeleo.com>
2018-07-04 17:33:10 +0200

    feature: add tracking

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,6 +7,17 @@ module.exports = {
   description: 'Embed dynamic timers that show real remaining time for your special event and get results.',
   serviceWorker: true, // automatically generate a service worker that caches the content for offline use (production)
   dest: 'dist',
+  head: [
+    ['script', {}, `
+      (function (w, d, t, u, n, a, m) {
+      w['MauticTrackingObject'] = n;
+        w[n] = w[n] || function () { (w[n].q = w[n].q || []).push(arguments) }, a = d.createElement(t),
+          m = d.getElementsByTagName(t)[0]; a.async = 1; a.src = u; m.parentNode.insertBefore(a, m)
+      })(window, document, 'script', 'https://emu-crm.westeurope.cloudapp.azure.com/mtc.js', 'mt');
+
+      mt('send', 'pageview');
+    `],
+  ],
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
